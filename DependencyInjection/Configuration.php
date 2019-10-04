5d29430023426cffef6c0bfe339d2e9658311817
ebukin <ebukin@accurateweb.ru>
2019-10-04 12:42:17 +0500

    *fix root configuration name

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -29,9 +29,7 @@ class Configuration implements ConfigurationInterface
   public function getConfigTreeBuilder ()
   {
     $treeBuilder = new TreeBuilder();
-    $rootNode = $treeBuilder->root('accurateweb_background_job');
-
-
+    $rootNode = $treeBuilder->root('accurateweb_task_scheduler');
 
     return $treeBuilder;
   }
